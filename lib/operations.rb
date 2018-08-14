9e10c8c6e7e44182526131f2be1f19643bdbe21b
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 22:18:05 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+	if (speed < 40) || (speed > 60)
+		true
+	elsif speed = 50
+		false
+	else
+		return
+ end
 end
+
 
 
 
 def not_safe?(speed)
-	
+	(speed < 40) || (speed > 60) ? true : false
 end
-	
-
-
